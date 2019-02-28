197654a38d4ff9c3501c24cc383cc1cff1c18b59
Ashay Changwani <ashaychangwani@gmail.com>
2019-02-28 20:46:55 +0530

    Ashay algo first commit:

---
+++ Algos/trail.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 10:13:46 2019
+
+@author: Ashay
+"""
+
---
+++ Algos/trial.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 10:13:46 2019
+
+@author: Ashay
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import confusion_matrix,accuracy_score
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, Activation
+from keras.callbacks import EarlyStopping
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+%matplotlib inline
+
+dataset_train=pd.read_csv('input/PM_train.txt',sep=' ',header=None).drop([26,27],axis=1)
+col_names = ['id','cycle','setting1','setting2','setting3','s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21']
+dataset_train.columns=col_names
+print('Shape of Train dataset: ',dataset_train.shape)
+dataset_train.head()
+
+
+dataset_test=pd.read_csv('input/PM_test.txt',sep=' ',header=None).drop([26,27],axis=1)
+dataset_test.columns=col_names
+#dataset_test.head()
+print('Shape of Test dataset: ',dataset_train.shape)
+dataset_train.head()
+
+
+pm_truth=pd.read_csv('input/PM_truth.txt',sep=' ',header=None).drop([1],axis=1)
+pm_truth.columns=['more']
+pm_truth['id']=pm_truth.index+1
+pm_truth.head()
+
+
+rul = pd.DataFrame(dataset_test.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+rul.head()
+
+
+pm_truth['rtf']=pm_truth['more']+rul['max']
+pm_truth.head()
+
+
+pm_truth.drop('more', axis=1, inplace=True)
+dataset_test=dataset_test.merge(pm_truth,on=['id'],how='left')
+dataset_test['ttf']=dataset_test['rtf'] - dataset_test['cycle']
+dataset_test.drop('rtf', axis=1, inplace=True)
+dataset_test.head()
+
+
+dataset_train['ttf'] = dataset_train.groupby(['id'])['cycle'].transform(max)-dataset_train['cycle']
+dataset_train.head()
+
+
+
+df_train=dataset_train.copy()
+df_test=dataset_test.copy()
+period=30
+df_train['label_bc'] = df_train['ttf'].apply(lambda x: 1 if x <= period else 0)
+df_test['label_bc'] = df_test['ttf'].apply(lambda x: 1 if x <= period else 0)
+df_train.head()
+
+
+features_col_name=['setting1', 'setting2', 'setting3', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11','s12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+target_col_name='label_bc'
+
+
+sc=MinMaxScaler()
+df_train[features_col_name]=sc.fit_transform(df_train[features_col_name])
+df_test[features_col_name]=sc.transform(df_test[features_col_name])
+
+
+
+def gen_sequence(id_df, seq_length, seq_cols):
+    df_zeros=pd.DataFrame(np.zeros((seq_length-1,id_df.shape[1])),columns=id_df.columns)
+    id_df=df_zeros.append(id_df,ignore_index=True)
+    data_array = id_df[seq_cols].values
+    num_elements = data_array.shape[0]
+    lstm_array=[]
+    for start, stop in zip(range(0, num_elements-seq_length), range(seq_length, num_elements)):
+        lstm_array.append(data_array[start:stop, :])
+    return np.array(lstm_array)
+
+# function to generate labels
+def gen_label(id_df, seq_length, seq_cols,label):
+    df_zeros=pd.DataFrame(np.zeros((seq_length-1,id_df.shape[1])),columns=id_df.columns)
+    id_df=df_zeros.append(id_df,ignore_index=True)
+    data_array = id_df[seq_cols].values
+    num_elements = data_array.shape[0]
+    y_label=[]
+    for start, stop in zip(range(0, num_elements-seq_length), range(seq_length, num_elements)):
+        y_label.append(id_df[label][stop])
+    return np.array(y_label)
+
+
+seq_length=50
+seq_cols=features_col_name
+
+X_train=np.concatenate(list(list(gen_sequence(df_train[df_train['id']==id], seq_length, seq_cols)) for id in df_train['id'].unique()))
+print(X_train.shape)
+# generate y_train
+y_train=np.concatenate(list(list(gen_label(df_train[df_train['id']==id], 50, seq_cols,'label_bc')) for id in df_train['id'].unique()))
+print(y_train.shape)
+
+
+X_test=np.concatenate(list(list(gen_sequence(df_test[df_test['id']==id], seq_length, seq_cols)) for id in df_test['id'].unique()))
+print(X_test.shape)
+# generate y_test
+y_test=np.concatenate(list(list(gen_label(df_test[df_test['id']==id], 50, seq_cols,'label_bc')) for id in df_test['id'].unique()))
+print(y_test.shape)
+
+
+nb_features =X_train.shape[2]
+timestamp=seq_length
+
+model = Sequential()
+
+model.add(LSTM(input_shape=(timestamp, nb_features), units=100,return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50,return_sequences=False))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.summary()
+
+
+import tensorflow as tf
+with tf.device('/device:GPU:0'):
+    model.fit(X_train, y_train, epochs=10, batch_size=200, validation_split=0.05, verbose=1,callbacks = [EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto')])
+
+
+scores = model.evaluate(X_train, y_train, verbose=1, batch_size=200)
+print('Accurracy: {0:.2f}%'.format(scores[1]*100))
+
+
+
+y_pred=model.predict_classes(X_test)
+print('Accuracy of model on test data: ',accuracy_score(y_test,y_pred))
+
+
+plt.plot(y_test, color='red', label='real')
+plt.show()
+plt.plot(y_pred, color='blue', label='predicted')
+plt.show()
+
+
+
+def prob_failure(machine_id):
+    machine_df=df_test[df_test.id==machine_id]
+    machine_test=gen_sequence(machine_df,seq_length,seq_cols)
+    m_pred=model.predict(machine_test)
+    failure_prob=list(m_pred[-1]*100)[0]
+    return failure_prob
+
+
+machine_id=3
+print('Probability that machine will fail within 30 days: {0:.2f}%'.format(prob_failure(machine_id)*100))
+
+    
