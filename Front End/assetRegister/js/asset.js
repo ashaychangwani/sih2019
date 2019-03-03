e3d3dbcb985382ea6fcafa70f30e8311aa0584e7
moksh98 <mokshjain98@gmail.com>
2019-03-03 06:33:54 +0530

    pre ashay

--- Front End/assetRegister/js/asset.js
+++ Front End/assetRegister/js/asset.js
@@ -1,46 +1,27 @@
-var idName2="test";
-function openCity(cityName, clicked_id2) {
+function myFunction(){
+    document.getElementById("myDIV").style.display="block";
+    document.getElementById("tab").style.display="block";
+}
+
+function clear() {
   var i;
-    document.getElementById("dets").style.display="none";
-    document.getElementById("preventive").style.display="none";
-    document.getElementById("history").style.display="none";
-  var x = document.getElementsByClassName("city");
+  var x = document.getElementsByClassName("tab");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-    
-  document.getElementById(cityName).style.display = "block";
-  idName2 = clicked_id2;
-    alert(idName2 + cityName);
 }
 
-var idName, length; 
-length = document.querySelectorAll('#history .table-row').length;
-console.log(length);
+function openDetails(){
+    clear();
+    document.getElementById("dets").style.display="block";
+}
 
-function reply_click(clicked_id)
-{
-    alert(clicked_id);
-    
-    idName = clicked_id;
-    console.log(idName);
-    document.getElementById(idName).addEventListener("click", function(){
-    for(var i = 0; i < length; i++){
-        alert("test"+idName+idName2);
-        document.getElementById("myDIV").style.display = "block";
-        document.getElementsByClassName("row1")[i].innerHTML = "";
-        document.getElementsByClassName("row1")[i].innerHTML = "London" + idName + idName2; //get from db
-        document.getElementsByClassName("row2")[i].innerHTML = "";
-        document.getElementsByClassName("row2")[i].innerHTML = "Paris" + idName + idName2;//get from db
-        document.getElementsByClassName("row3")[i].innerHTML = "";
-        document.getElementsByClassName("row3")[i].innerHTML = "Tokyo" + idName + idName2;//get from db
-        document.getElementsByClassName("row4")[i].innerHTML = "";
-        document.getElementsByClassName("row4")[i].innerHTML = "Tokyo" + idName + idName2;//get from db
-        document.getElementsByClassName("row5")[i].innerHTML = "";
-        document.getElementsByClassName("row5")[i].innerHTML = "Tokyo" + idName + idName2;//get from db
-        document.getElementsByClassName("row6")[i].innerHTML = "";
-        document.getElementsByClassName("row6")[i].innerHTML = "Tokyo" + idName + idName2;//get from db
-    }
-});
+function openPreventive(){
+    clear();
+    document.getElementById("preventive").style.display="block";
+}
 
+function openHistory(){
+    clear();
+    document.getElementById("history").style.display="block";
 }
